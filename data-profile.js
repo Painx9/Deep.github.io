3cbc9070bc47a5ee6f522bbe741a0eb494ace06a
Deep Patel <DP1852001@GMAIL.COM>
2026-07-22 02:09:09 +0200

    Add user profile data to data-profile.js

---
+++ data-profile.js
@@ -0,0 +1,12 @@
+const userProfile = {
+    username: "Painx9",
+    name: "Deep Patel",
+    alias: "PA1N",
+    location: "Hamburg / Cottbus, Germany",
+    email: "dp1852001@gmail.com",
+    phone: "+49 155 6027 5161",
+    github: "https://github.com/Painx9",
+    linkedin: "https://www.linkedin.com/in/deepatel009/",
+    discord: "https://discordapp.com/users/akaxpain",
+    instagram: "https://www.instagram.com/deepatel.de?igsh=NHA3dXRydXRmMDM2&utm_source=qr"
+};
